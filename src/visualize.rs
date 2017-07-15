eb4efd28b64e11c1b390ebd46cf2167fe155277e
Andrey Lesnikov <ozkriff@gmail.com>
2017-07-15 18:54:45 +0300

    #7: get real blocking time from 'visualize' func

--- src/visualize.rs
+++ src/visualize.rs
@@ -14,19 +14,25 @@ pub fn visualize(
     view: &mut GameView,
     context: &mut Context,
     event: &Event,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
+    let mut total_duration = Time(0.0);
     let mut actions: Vec<Box<Action>> = Vec::new();
-    actions.push(visualize_event(state, view, context, &event.active_event));
+    let (event_action, event_time) = visualize_event(state, view, context, &event.active_event);
+    total_duration.0 += event_time.0;
+    actions.push(event_action);
     for (&target_id, effects) in &event.effects {
         for effect in effects {
-            actions.push(visualize_effect(state, view, context, target_id, effect));
+            let (action, duration) = visualize_effect(state, view, context, target_id, effect);
+            total_duration.0 += duration.0;
+            actions.push(action);
         }
     }
     let mut forked_actions: Vec<Box<Action>> = Vec::new();
     for action in actions {
         forked_actions.push(Box::new(action::Fork::new(action)));
     }
-    Box::new(action::Sequence::new(forked_actions))
+    let final_action = Box::new(action::Sequence::new(forked_actions));
+    (final_action, total_duration)
 }
 
 pub fn visualize_event(
@@ -34,7 +40,7 @@ pub fn visualize_event(
     view: &mut GameView,
     context: &mut Context,
     event: &ActiveEvent,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     match *event {
         ActiveEvent::Create(ref event) => visualize_event_create(state, view, context, event),
         ActiveEvent::MoveTo(ref event) => visualize_event_move_to(state, view, context, event),
@@ -47,7 +53,7 @@ fn visualize_event_create(
     view: &mut GameView,
     context: &mut Context,
     event: &event::Create,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     let point = map::hex_to_point(view.tile_size(), event.unit.pos);
     let sprite_name = match event.unit.player_id {
         PlayerId(0) => "swordsman.png",
@@ -58,14 +64,16 @@ fn visualize_event_create(
     sprite.set_color([1.0, 1.0, 1.0, 0.0]);
     sprite.set_pos(point);
     view.add_object(event.id, &sprite);
-    Box::new(action::Sequence::new(vec![
+    let action = Box::new(action::Sequence::new(vec![
         Box::new(action::Show::new(&view.layers().fg, &sprite)),
         Box::new(action::ChangeColorTo::new(
             &sprite,
             [1.0, 1.0, 1.0, 1.0],
             Time(0.5),
         )),
-    ]))
+    ]));
+    let time = action.duration();
+    (action, time)
 }
 
 fn visualize_event_move_to(
@@ -73,7 +81,7 @@ fn visualize_event_move_to(
     view: &mut GameView,
     _: &mut Context,
     event: &event::MoveTo,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     let sprite = view.id_to_sprite(event.id).clone();
     let mut actions: Vec<Box<Action>> = Vec::new();
     // TODO: add Path struct with `iter` method returning
@@ -84,7 +92,9 @@ fn visualize_event_move_to(
         let diff = Point(to.0 - from.0);
         actions.push(Box::new(action::MoveBy::new(&sprite, diff, Time(0.3))));
     }
-    Box::new(action::Sequence::new(actions))
+    let action = Box::new(action::Sequence::new(actions));
+    let time = action.duration();
+    (action, time)
 }
 
 fn visualize_event_attack(
@@ -92,16 +102,18 @@ fn visualize_event_attack(
     view: &mut GameView,
     _: &mut Context,
     event: &event::Attack,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     let sprite = view.id_to_sprite(event.attacker_id).clone();
     let map_to = state.unit(event.target_id).pos;
     let to = map::hex_to_point(view.tile_size(), map_to);
     let from = sprite.pos();
     let diff = Point((to.0 - from.0) / 2.0);
-    Box::new(action::Sequence::new(vec![
+    let action = Box::new(action::Sequence::new(vec![
         Box::new(action::MoveBy::new(&sprite, diff, Time(0.15))),
         Box::new(action::MoveBy::new(&sprite, Point(-diff.0), Time(0.15))),
-    ]))
+    ]));
+    let time = action.duration();
+    (action, time)
 }
 
 pub fn visualize_effect(
@@ -110,7 +122,7 @@ pub fn visualize_effect(
     context: &mut Context,
     target_id: ObjId,
     effect: &Effect,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     match *effect {
         Effect::Kill => visualize_effect_kill(state, view, context, target_id),
     }
@@ -121,13 +133,15 @@ fn visualize_effect_kill(
     view: &mut GameView,
     _: &mut Context,
     target_id: ObjId,
-) -> Box<Action> {
+) -> (Box<Action>, Time) {
     let sprite = view.id_to_sprite(target_id).clone();
     view.remove_object(target_id);
     let color = [1.0, 1.0, 1.0, 0.0];
-    Box::new(action::Sequence::new(vec![
+    let action = Box::new(action::Sequence::new(vec![
         Box::new(action::Sleep::new(Time(0.25))),
         Box::new(action::ChangeColorTo::new(&sprite, color, Time(0.10))),
         Box::new(action::Hide::new(&view.layers().fg, &sprite)),
-    ]))
+    ]));
+    let time = action.duration();
+    (action, time)
 }
