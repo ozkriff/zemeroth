52613e49e354207ce61af4c3d186899754296e50
Andrey Lesnikov <ozkriff@gmail.com>
2017-08-24 20:18:48 +0300

    #73: Grass

--- src/game_view.rs
+++ src/game_view.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
+use rand::{thread_rng, Rng};
+use cgmath::Vector2;
 use hate::{Context, Scene, Sprite, Time};
 use hate::scene::action;
 use hate::scene::{Action, Layer};
+use hate::geom::Point;
 use map;
 use core::{ObjId, State, TileType};
 
 #[derive(Debug, Clone, Default)]
 pub struct Layers {
     pub bg: Layer,
+    pub grass: Layer,
     pub walkable_tiles: Layer,
     pub attackable_tiles: Layer,
     pub selection_marker: Layer,
@@ -19,6 +23,7 @@ impl Layers {
     fn sorted(self) -> Vec<Layer> {
         vec![
             self.bg,
+            self.grass,
             self.walkable_tiles,
             self.attackable_tiles,
             self.selection_marker,
@@ -85,6 +90,7 @@ impl GameView {
 
 // TODO: move to game.rs?
 fn make_map_action(state: &State, view: &GameView, context: &mut Context) -> Box<Action> {
+    let mut rng = thread_rng();
     let mut actions: Vec<Box<Action>> = Vec::new();
     for hex_pos in state.map().iter() {
         let screen_pos = map::hex_to_point(view.tile_size(), hex_pos);
@@ -95,6 +101,16 @@ fn make_map_action(state: &State, view: &GameView, context: &mut Context) -> Box
         }
         sprite.set_pos(screen_pos);
         actions.push(Box::new(action::Show::new(&view.layers.bg, &sprite)));
+        if rng.gen_range(0, 10) < 2 {
+            let mut sprite = Sprite::from_path(context, "grass.png", view.tile_size() * 2.0);
+            let n = view.tile_size() / 2.0;
+            let screen_pos_grass = Point(Vector2 {
+                x: screen_pos.0.x + rng.gen_range(-n, n),
+                y: screen_pos.0.y + rng.gen_range(-n, n),
+            });
+            sprite.set_pos(screen_pos_grass);
+            actions.push(Box::new(action::Show::new(&view.layers.grass, &sprite)));
+        }
     }
     Box::new(action::Sequence::new(actions))
 }
