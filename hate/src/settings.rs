291a59190d02c3f4d7ddd1bf740b9ce21415f2bb
Andrey Lesnikov <ozkriff@gmail.com>
2017-10-04 01:17:56 +0300

    #137: Added Default impl for hate::Settings

--- hate/src/settings.rs
+++ hate/src/settings.rs
@@ -1,9 +1,22 @@
+use std::default::Default;
 use std::path::PathBuf;
 
 #[derive(Debug, Clone, Deserialize)]
+#[serde(default)]
 pub struct Settings {
     pub tap_tolerance: f32,
     pub text_texture_height: f32,
     pub font: Option<PathBuf>,
     pub max_fps: f32,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            tap_tolerance: 0.05,
+            text_texture_height: 80.0,
+            font: None, // TODO: None -> "<embedded>"
+            max_fps: 60.0,
+        }
+    }
 }
