56dc8ddc6b2b034216500191043cff7bae4e0a6d
Andrey Lesnikov <ozkriff@gmail.com>
2017-10-20 09:20:38 +0300

    Moved select/deselect logic to GameView

--- src/game_view.rs
+++ src/game_view.rs
@@ -1,7 +1,15 @@
 use std::collections::HashMap;
 use hate::{Context, Scene, Sprite, Time};
-use hate::scene::{Action, Layer};
+use hate::scene::Layer;
+use hate::scene::action::{self, Action};
+use core::{Jokers, Moves, State, check};
 use core::ObjId;
+use core::map::HexMap;
+use core::movement::Tile;
+use core::command;
+use map::hex_to_point;
+
+const WALKBALE_TILE_COLOR: [f32; 4] = [0.2, 1.0, 0.2, 0.5];
 
 #[derive(Debug, Clone, Default)]
 pub struct Layers {
@@ -31,26 +39,41 @@ impl Layers {
 }
 
 #[derive(Debug)]
+struct Sprites {
+    selection_marker: Sprite,
+    walkable_tiles: Vec<Sprite>,
+    attackable_tiles: Vec<Sprite>,
+    id_to_sprite_map: HashMap<ObjId, Sprite>,
+    unit_info: HashMap<ObjId, Vec<Sprite>>,
+}
+
+#[derive(Debug)]
 pub struct GameView {
     tile_size: f32,
     layers: Layers,
-    obj_to_sprite_map: HashMap<ObjId, Sprite>,
     scene: Scene,
-    sprites_unit_info: HashMap<ObjId, Vec<Sprite>>,
+    sprites: Sprites,
 }
 
 impl GameView {
-    pub fn new() -> Self {
-        let obj_to_sprite_map = HashMap::new();
+    pub fn new(context: &mut Context) -> Self {
         let layers = Layers::default();
         let scene = Scene::new(layers.clone().sorted());
-        let tile_size = 0.11;
+        let tile_size = 0.11; // TODO: Read from the config?
+        let mut selection_marker = Sprite::from_path(context, "selection.png", tile_size * 2.0);
+        selection_marker.set_color([0.0, 0.0, 1.0, 0.8]);
+        let sprites = Sprites {
+            selection_marker,
+            walkable_tiles: Vec::new(),
+            attackable_tiles: Vec::new(),
+            id_to_sprite_map: HashMap::new(),
+            unit_info: HashMap::new(),
+        };
         Self {
             scene,
             tile_size,
             layers,
-            obj_to_sprite_map,
-            sprites_unit_info: HashMap::new(),
+            sprites,
         }
     }
 
@@ -72,26 +95,136 @@ impl GameView {
     }
 
     pub fn add_object(&mut self, id: ObjId, sprite: &Sprite) {
-        self.obj_to_sprite_map.insert(id, sprite.clone());
+        self.sprites.id_to_sprite_map.insert(id, sprite.clone());
     }
 
     pub fn remove_object(&mut self, id: ObjId) {
-        self.obj_to_sprite_map.remove(&id).unwrap();
+        self.sprites.id_to_sprite_map.remove(&id).unwrap();
     }
 
     pub fn id_to_sprite(&mut self, id: ObjId) -> &Sprite {
-        &self.obj_to_sprite_map[&id]
+        &self.sprites.id_to_sprite_map[&id]
     }
 
     pub fn unit_info_check(&self, id: ObjId) -> bool {
-        self.sprites_unit_info.get(&id).is_some()
+        self.sprites.unit_info.get(&id).is_some()
     }
 
     pub fn unit_info_get(&mut self, id: ObjId) -> Vec<Sprite> {
-        self.sprites_unit_info.remove(&id).unwrap()
+        self.sprites.unit_info.remove(&id).unwrap()
     }
 
     pub fn unit_info_set(&mut self, id: ObjId, sprites: Vec<Sprite>) {
-        self.sprites_unit_info.insert(id, sprites);
+        self.sprites.unit_info.insert(id, sprites);
+    }
+
+    pub fn deselect(&mut self) {
+        let action_hide = Box::new(action::Hide::new(
+            &self.layers.selection_marker,
+            &self.sprites.selection_marker,
+        ));
+        self.add_action(action_hide);
+        for sprite in self.sprites.walkable_tiles.split_off(0) {
+            let mut color = WALKBALE_TILE_COLOR;
+            color[3] = 0.0;
+            let action = {
+                let layer = &self.layers().walkable_tiles;
+                Box::new(action::Sequence::new(vec![
+                    Box::new(action::ChangeColorTo::new(&sprite, color, Time(0.2))),
+                    Box::new(action::Hide::new(layer, &sprite)),
+                ]))
+            };
+            self.add_action(action);
+        }
+        for sprite in self.sprites.attackable_tiles.split_off(0) {
+            let action = {
+                let layer = &self.layers().attackable_tiles;
+                Box::new(action::Hide::new(layer, &sprite))
+            };
+            self.add_action(action);
+        }
+    }
+
+    pub fn select_unit(
+        &mut self,
+        state: &State,
+        map: &HexMap<Tile>,
+        context: &mut Context,
+        id: ObjId,
+    ) {
+        self.show_selection_marker(state, id);
+        self.show_walkable_tiles(state, map, context, id);
+        self.show_attackable_tiles(state, context, id);
+    }
+
+    fn show_selection_marker(&mut self, state: &State, id: ObjId) {
+        let pos = state.parts().pos.get(id).0;
+        let point = hex_to_point(self.tile_size(), pos);
+        self.sprites.selection_marker.set_pos(point);
+        let action = Box::new(action::Show::new(
+            &self.layers().selection_marker,
+            &self.sprites.selection_marker,
+        ));
+        self.add_action(action);
+    }
+
+    fn show_attackable_tiles(&mut self, state: &State, context: &mut Context, id: ObjId) {
+        let parts = state.parts();
+        let selected_unit_player_id = parts.belongs_to.get(id).0;
+        for target_id in parts.agent.ids() {
+            let target_pos = parts.pos.get(target_id).0;
+            let target_player_id = parts.belongs_to.get(target_id).0;
+            if target_player_id == selected_unit_player_id {
+                continue;
+            }
+            let command_attack = command::Command::Attack(command::Attack {
+                attacker_id: id,
+                target_id: target_id,
+            });
+            if check(state, &command_attack).is_err() {
+                continue;
+            }
+            let size = self.tile_size() * 2.0;
+            let mut sprite = Sprite::from_path(context, "tile.png", size);
+            self.sprites.attackable_tiles.push(sprite.clone());
+            sprite.set_color([1.0, 0.3, 0.3, 0.8]);
+            sprite.set_pos(hex_to_point(self.tile_size(), target_pos));
+            let action = Box::new(action::Show::new(
+                &self.layers().attackable_tiles,
+                &sprite,
+            ));
+            self.add_action(action);
+        }
+    }
+
+    fn show_walkable_tiles(
+        &mut self,
+        state: &State,
+        map: &HexMap<Tile>,
+        context: &mut Context,
+        id: ObjId,
+    ) {
+        let agent = state.parts().agent.get(id);
+        if agent.moves == Moves(0) && agent.jokers == Jokers(0) {
+            return;
+        }
+        for pos in map.iter() {
+            if map.tile(pos).cost() > agent.move_points {
+                continue;
+            }
+            let size = self.tile_size() * 2.0;
+            let mut sprite = Sprite::from_path(context, "tile.png", size);
+            self.sprites.walkable_tiles.push(sprite.clone());
+            let mut color_from = WALKBALE_TILE_COLOR;
+            color_from[3] = 0.0;
+            sprite.set_color(color_from);
+            sprite.set_pos(hex_to_point(self.tile_size(), pos));
+            let color_to = WALKBALE_TILE_COLOR;
+            let action = Box::new(action::Sequence::new(vec![
+                Box::new(action::Show::new(&self.layers().walkable_tiles, &sprite)),
+                Box::new(action::ChangeColorTo::new(&sprite, color_to, Time(0.2))),
+            ]));
+            self.add_action(action);
+        }
     }
 }
--- src/screen/game.rs
+++ src/screen/game.rs
@@ -9,7 +9,7 @@ use visualize;
 use map;
 use game_view::GameView;
 use ai::Ai;
-use core::{self, check, Jokers, Moves, ObjId, PlayerId, State, TileType};
+use core::{self, check, ObjId, PlayerId, State, TileType};
 use core::command;
 use core::execute;
 use core::map::PosHex;
@@ -21,8 +21,6 @@ enum GuiCommand {
     Deselect,
     EndTurn,
 }
-
-const WALKBALE_TILE_COLOR: [f32; 4] = [0.2, 1.0, 0.2, 0.5];
 
 fn make_action_show_tile(
     context: &mut Context,
@@ -153,10 +151,7 @@ pub struct Game {
     view: GameView,
     selected_unit_id: Option<ObjId>,
     pathfinder: Pathfinder,
-    sprite_selection_marker: Sprite,
     block_timer: Option<Time>,
-    sprites_walkable_tiles: Vec<Sprite>, // TODO: move to GameView
-    sprites_attackable_tiles: Vec<Sprite>, // TODO: move to GameView
     ai: Ai,
     layout_id_info: Option<gui::Id>,
 }
@@ -168,11 +163,8 @@ impl Game {
         debug!("{:?}", prototypes);
         let mut state = State::new(prototypes);
         let radius = state.map().radius();
-        let mut view = GameView::new();
+        let mut view = GameView::new(context);
         prepare_map_and_state(context, &mut state, &mut view);
-        let size = view.tile_size() * 2.0;
-        let mut sprite_selection_marker = Sprite::from_path(context, "selection.png", size);
-        sprite_selection_marker.set_color([0.0, 0.0, 1.0, 0.8]);
         Self {
             gui: build_gui(context),
             state,
@@ -180,9 +172,6 @@ impl Game {
             selected_unit_id: None,
             pathfinder: Pathfinder::new(radius),
             block_timer: None,
-            sprite_selection_marker,
-            sprites_walkable_tiles: Vec::new(),
-            sprites_attackable_tiles: Vec::new(),
             ai: Ai::new(PlayerId(1), radius),
             layout_id_info: None,
         }
@@ -196,7 +185,7 @@ impl Game {
         if self.block_timer.is_some() {
             return;
         }
-        self.deselect(context);
+        self.deselect();
         let command = command::Command::EndTurn(command::EndTurn);
         let mut actions = Vec::new();
         actions.push(self.do_command_inner(context, command));
@@ -224,7 +213,7 @@ impl Game {
         while let Some(command) = self.gui.try_recv() {
             match command {
                 GuiCommand::Exit => self.exit(context),
-                GuiCommand::Deselect => self.deselect(context),
+                GuiCommand::Deselect => self.deselect(),
                 GuiCommand::EndTurn => self.end_turn(context),
             }
         }
@@ -259,116 +248,22 @@ impl Game {
         self.view.add_action(action);
     }
 
-    fn show_selection_marker(&mut self, id: ObjId) {
-        let pos = self.state.parts().pos.get(id).0;
-        let point = map::hex_to_point(self.view.tile_size(), pos);
-        self.sprite_selection_marker.set_pos(point);
-        let action = Box::new(action::Show::new(
-            &self.view.layers().selection_marker,
-            &self.sprite_selection_marker,
-        ));
-        self.view.add_action(action);
-    }
-
-    fn show_attackable_tiles(&mut self, context: &mut Context, id: ObjId) {
-        let parts = self.state.parts();
-        let selected_unit_player_id = parts.belongs_to.get(id).0;
-        for target_id in parts.agent.ids() {
-            let target_pos = parts.pos.get(target_id).0;
-            let target_player_id = parts.belongs_to.get(target_id).0;
-            if target_player_id == selected_unit_player_id {
-                continue;
-            }
-            let command_attack = command::Command::Attack(command::Attack {
-                attacker_id: id,
-                target_id: target_id,
-            });
-            if check(&self.state, &command_attack).is_err() {
-                continue;
-            }
-            let size = self.view.tile_size() * 2.0;
-            let mut sprite = Sprite::from_path(context, "tile.png", size);
-            self.sprites_attackable_tiles.push(sprite.clone());
-            sprite.set_color([1.0, 0.3, 0.3, 0.8]);
-            sprite.set_pos(map::hex_to_point(self.view.tile_size(), target_pos));
-            let action = Box::new(action::Show::new(
-                &self.view.layers().attackable_tiles,
-                &sprite,
-            ));
-            self.view.add_action(action);
-        }
-    }
-
-    fn show_walkable_tiles(&mut self, context: &mut Context, id: ObjId) {
-        let agent = self.state.parts().agent.get(id);
-        if agent.moves == Moves(0) && agent.jokers == Jokers(0) {
-            return;
-        }
-        let map = self.pathfinder.map();
-        for pos in map.iter() {
-            let tile = map.tile(pos);
-            if tile.cost() <= agent.move_points {
-                let size = self.view.tile_size() * 2.0;
-                let mut sprite = Sprite::from_path(context, "tile.png", size);
-                self.sprites_walkable_tiles.push(sprite.clone());
-                let mut color_from = WALKBALE_TILE_COLOR;
-                color_from[3] = 0.0;
-                sprite.set_color(color_from);
-                sprite.set_pos(map::hex_to_point(self.view.tile_size(), pos));
-                let color_to = WALKBALE_TILE_COLOR;
-                let action = {
-                    let layer = &self.view.layers().walkable_tiles;
-                    Box::new(action::Sequence::new(vec![
-                        Box::new(action::Show::new(layer, &sprite)),
-                        Box::new(action::ChangeColorTo::new(&sprite, color_to, Time(0.2))),
-                    ]))
-                };
-                self.view.add_action(action);
-            }
-        }
-    }
-
-    fn deselect(&mut self, _: &mut Context) {
+    fn deselect(&mut self) {
         if let Some(layout_id_info) = self.layout_id_info.take() {
             self.gui.remove(layout_id_info).unwrap();
         }
         if self.selected_unit_id.is_some() {
-            let action_hide = Box::new(action::Hide::new(
-                &self.view.layers().selection_marker,
-                &self.sprite_selection_marker,
-            ));
-            self.view.add_action(action_hide);
-            for sprite in self.sprites_walkable_tiles.split_off(0) {
-                let mut color = WALKBALE_TILE_COLOR;
-                color[3] = 0.0;
-                let action = {
-                    let layer = &self.view.layers().walkable_tiles;
-                    Box::new(action::Sequence::new(vec![
-                        Box::new(action::ChangeColorTo::new(&sprite, color, Time(0.2))),
-                        Box::new(action::Hide::new(layer, &sprite)),
-                    ]))
-                };
-                self.view.add_action(action);
-            }
-            for sprite in self.sprites_attackable_tiles.split_off(0) {
-                let action = {
-                    let layer = &self.view.layers().attackable_tiles;
-                    Box::new(action::Hide::new(layer, &sprite))
-                };
-                self.view.add_action(action);
-            }
+            self.view.deselect();
         }
         self.selected_unit_id = None;
     }
 
     fn select_unit(&mut self, context: &mut Context, id: ObjId) {
-        self.deselect(context);
+        self.deselect();
         assert!(self.state.parts().agent.get_opt(id).is_some());
         self.selected_unit_id = Some(id);
         self.pathfinder.fill_map(&self.state, id);
-        self.show_selection_marker(id);
-        self.show_walkable_tiles(context, id);
-        self.show_attackable_tiles(context, id);
+        self.view.select_unit(&self.state, self.pathfinder.map(), context, id);
         {
             let gui = &mut self.gui;
             let layout_id_info = build_unit_info_panel(context, gui, &self.state, id);
@@ -397,7 +292,7 @@ impl Game {
                     let selected_unit_player_id =
                         self.state.parts().belongs_to.get(selected_unit_id).0;
                     if selected_unit_id == id {
-                        self.deselect(context);
+                        self.deselect();
                         return;
                     }
                     if other_unit_player_id == selected_unit_player_id
@@ -444,7 +339,7 @@ impl Game {
                     if self.state.parts().agent.get_opt(id).is_some() {
                         self.select_unit(context, id);
                     } else {
-                        self.deselect(context);
+                        self.deselect();
                     }
                 }
             }
