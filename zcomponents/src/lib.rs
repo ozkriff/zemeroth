07a6231f90bf914f8250a29405fc6f5663b04d57
Luke Schoen <ltfschoen@users.noreply.github.com>
2019-06-11 01:08:53 +0200

    docs: Rephrase again

--- zcomponents/src/lib.rs
+++ zcomponents/src/lib.rs
@@ -133,8 +133,8 @@ impl<Id: Hash + Eq + Copy + Debug, V: Clone> ComponentContainer<Id, V> {
             .unwrap_or_else(|| panic!("Can't find {:?} id", id))
     }
 
-    /// Insert a given data value into a given entity id of a stupid component
-    /// if no value is stored under that entity's id.
+    /// Store a given data value under a given entity id of a stupid component
+    /// if no value is already stored under that entity's id.
     pub fn insert(&mut self, id: Id, data: V) {
         assert!(self.get_opt(id).is_none());
         self.data.insert(id, data);
