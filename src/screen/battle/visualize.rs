8b4a268d0f71459d875a3863755c5fa8a68f8e51
Andrey Lesnikov <ozkriff@gmail.com>
2020-07-20 21:33:43 +0300

    Change color of joker dots from purple to white

--- src/screen/battle/visualize.rs
+++ src/screen/battle/visualize.rs
@@ -427,7 +427,7 @@ fn generate_brief_obj_info(
             ([0.3, 0.5, 0.3, 0.5], damage),
             ([1.0, 1.0, 0.0, 1.0], armor.0),
         ],
-        &[([0.9, 0.1, 0.9, 1.0], agent.jokers.0)],
+        &[([1.0, 1.0, 1.0, 1.0], agent.jokers.0)],
         &[([1.0, 0.0, 0.0, 1.0], agent.attacks.0)],
         &[([0.0, 0.0, 1.0, 1.0], agent.moves.0)],
     ];
