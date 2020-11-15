2dd85129d54460b844f4a746c9b97804a6a99012
Andrey Lesnikov <ozkriff@gmail.com>
2020-11-15 13:16:42 +0300

    WIP: revive wasm build

--- utils/wasm/build.sh
+++ utils/wasm/build.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-cd assets && tar cf assets.tar * && cd .. && mv -f assets/assets.tar .
-
 cargo build --target wasm32-unknown-unknown --release
 
 rm -rf static
 mkdir static
+cp -r assets static/assets
 cp target/wasm32-unknown-unknown/release/zemeroth.wasm static/
 cp utils/wasm/index.html static/
 ls -lh static
